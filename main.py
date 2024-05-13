532df3092d46b1b2353cb0485fe479c66ec8fd49
orangepi <cyrusxc5@gmail.com>
2024-05-13 18:52:03 -0400

    pi

--- main.py
+++ main.py
@@ -1,15 +1,16 @@
 from pygemma import Gemma, ModelType, ModelTraining
 import speech.cloud_api as api
 import re
+import time
 
 # gemma.show_help()
 # gemma.show_config()
 gemma = Gemma(
-	tokenizer_path="gemma-cpp/build/tokenizer.spm",
-	compressed_weights_path="gemma-cpp/build/2b-it-sfp.sbs",
-	# compressed_weights_path="gemma-cpp/build/params.pkl",
-	model_type=ModelType.Gemma2B,
-	model_training=ModelTraining.GEMMA_IT,
+    tokenizer_path="gemma-cpp/build/tokenizer.spm",
+    compressed_weights_path="gemma-cpp/build/2b-it-sfp.sbs",
+    # compressed_weights_path="gemma-cpp/build/params.pkl",
+    model_type=ModelType.Gemma2B,
+    model_training=ModelTraining.GEMMA_IT,
 )
 
 project_id = "gemma-speech-420819"
@@ -18,18 +19,18 @@ language_codes = ["zh-Hans-CN", "hi-IN"]
 # Main loop
 while True:
     api.beep()
-    duration = 5
+    duration = 4
     audio_file = "outputs/audio.wav"  # Define the path to save recorded audio
     api.record_audio(duration, audio_file)  # Record audio
+    start_time = time.time()
     print("Transcribing audio...")
     response, languages = api.cloud_STT(audio_file)  # Transcribe speech to text
     print("Transcribed text:", response)
     print("Detected languages:", languages)
-    
-    
+
     # Determine the translation prompt based on the detected language
     lang = 0
-    
+
     if "hi-in" in languages:
         lang = 0
         # prompt = f"Translate this Hindi to Chinese, don't give any explaination:\n{response[0]}\n"
@@ -40,23 +41,30 @@ while True:
         prompt = f"Translate this Chinese sentence to English and Hindi in this format ('hindi': ' ', 'english: ' ')\n{response[0]}\n"
     else:
         prompt = None
-        
+
     if prompt:
         # Feed the prompt to the model and get the translated text
-        translated_text = gemma(prompt)
-        print("Translated text:", translated_text)
-        
+        start_gemma_time = time.time()
+        translated_text = gemma(prompt) + "\n"
+        end_gemma_time = time.time()
+        print("Translated text:", translated_text, "\n in ", end_gemma_time-start_gemma_time, "seconds\n")
+
         # Convert translated text to speech
-	matches = ""
-	if lang == 0:
-		pattern_chinese = r"\*\* Chinese: \*\* (.+?)\*\*"
-		matches = re.findall(pattern_chinese, translated_text, re.DOTALL)
-	else:
-		pattern_hindi = r"\*\* Hindi: \*\* (.+?)\*\*"
-		matches = re.findall(pattern_hindi, translated_text, re.DOTALL)
-        audio_output = api.cloud_TTS(matches, language_codes[lang])
-        
+        matches = ""
+        if lang == 0:
+            pattern_chinese = r"\*\*Chinese:\*\* (.+)\n"
+            matches = re.findall(pattern_chinese, translated_text, re.DOTALL)
+        else:
+            pattern_hindi = r"\*\*Hindi:\*\* (.+)\n"
+            matches = re.findall(pattern_hindi, translated_text, re.DOTALL)
+
+        audio_output = api.cloud_TTS("output" + matches[0], language_codes[lang])
+
         # Play the generated audio
+        end_time = time.time()
         api.play_audio(audio_output)
+        
+        # measure time taken for whole loop
+        print("Time taken for a whole loop:", end_time - start_time, "seconds\n\n")
     else:
         print("Language not supported for translation.")
